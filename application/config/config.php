9aa8a8e7208ffb974576613217543aa0d198bc6c
fabianelias <fabian_bravo@live.com>
2018-05-23 00:45:22 -0400

    mejoras en las rutas

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['name_app'] = 'Soplón';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
--- application/controllers/app/Home.php
+++ application/controllers/app/Home.php
@@ -7,22 +7,13 @@ class Home extends CI_controller{
     $this->load->model('pages_model');
   }
 
-  public function index(){
-
-    $data = ['title_page' => 'Soplón app'];
-    $this->load->view('includes/head', $data)
-               ->view('includes/nav')
-               ->view('home/index')
-               ->view('includes/footer_view', $data);
-  }
-
   public function categories($category){
 
     $data = ['title_page'=> ucfirst($category). ' - Soplón', 'category' => $category];
 
     // información de la categoria
     $data['links'] = $this->pages_model->get_pages($category);
-    //echo "<pre>"; print_r($data['links']);die();
+    
     $this->load->view('includes/head', $data)
                ->view('includes/nav')
                ->view('category/index', $data)
